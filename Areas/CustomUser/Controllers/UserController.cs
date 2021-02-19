49b422f16e39c9ef4a43671bcf931f3519b4ca01
Parry935 <dominikpepas@gmail.com>
2021-02-20 00:44:34 +0100

    Add personal informations about user functionality

---
+++ Areas/CustomUser/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Forum.Interfaces.Data;
+using Forum.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Areas.CustomUser.Controllers
+{
+
+    [Area("CustomUser")]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index(string name)
+        {
+            var user = await _unitOfWork.User.GetFirstOrDefaultAsync(m=>m.UserName == name);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            var userPosts = await _unitOfWork.Post.GetAllAsync(m => m.UserId == user.Id, "Topic,Likes", m=>m.OrderByDescending(x =>x.PostDate));
+
+            var cntUserTopics = _unitOfWork.Topic.GetAllAsync(m => m.UserId == user.Id).Result.Count();
+
+            var cntUserPosts = userPosts.Count();
+
+            int reputation = 0;
+
+            foreach (var post in userPosts)
+            {
+                reputation += post.Likes.Count();
+            }
+
+            var userProfile = new UserProfileVM()
+            {
+                User = user,
+                CountTopics = cntUserTopics,
+                CountPosts = cntUserPosts,
+                Reputation = reputation,
+                RecentPosts = userPosts.Take(3)
+            };
+
+            return View(userProfile);
+        }
+    }
+}
---
+++ ViewModel/UserProfileVM.cs
@@ -0,0 +1,17 @@
+﻿using Forum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.ViewModel
+{
+    public class UserProfileVM
+    {
+        public User User { get; set; }
+        public int CountTopics { get; set; }
+        public int CountPosts { get; set; }
+        public int Reputation { get; set; }
+        public IEnumerable<Post> RecentPosts { get; set; }
+    }
+}
